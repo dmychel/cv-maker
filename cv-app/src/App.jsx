0ff1ed39d688e0abb4c8e661546a4bcb9383b589
dmychel <dmychel@tutanota.com>
2023-12-13 19:49:13 -0500

    Minimal useState
    Created the minimum useState data to make the Form component interactable

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import Forms from "./components/forms/Forms";
 import Resume from "./components/resume/Resume";
 
 function App() {
+  const [personal, setPersonal] = useState({});
+  const [experience, setExperience] = useState([]);
+  const [education, setEducation] = useState([]);
+
   return (
     <>
       <section className="app">
-        <Forms />
-        <Resume />
+        <Forms
+          setPersonal={setPersonal}
+          experience={experience}
+          setExperience={setExperience}
+          education={education}
+          setEducation={setEducation}
+        />
+        <Resume personal={personal} />
       </section>
     </>
   );
