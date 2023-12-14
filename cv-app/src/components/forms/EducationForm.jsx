5b0c762b84b9c316155d31fa193f2990c4ffb3ed
dmychel <dmychel@tutanota.com>
2023-12-13 19:45:47 -0500

    functional forms
    Set up useState so the forms are reactive. When each form is submitted, the submitHandler function creates an object and uses useState function to store it in App.jsx

--- cv-app/src/components/forms/EducationForm.jsx
+++ cv-app/src/components/forms/EducationForm.jsx
@@ -1,20 +1,52 @@
-const EducationForm = () => {
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const EducationForm = ({ education, setEducation }) => {
+  const [uni, setUni] = useState("");
+  const [achieve, setAchieve] = useState("");
+  const [date, setDate] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newObject = {
+      uni: uni,
+      achieve: achieve,
+      date: date,
+    };
+    setEducation([...education, newObject]);
+  };
+
   return (
     <>
       <div className="form-container">
         <h2>Education</h2>
         <div className="form-header">
-          <h3>Company Name</h3>
+          <h3>Academic Accolade</h3>
           <button className="open">&#8226;</button>
           <button className="delete">&#x2715;</button>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="uni">University</label>
-          <input type="text" name="uni" />
-          <label htmlFor="acheivement">Acheivement</label>
-          <input type="text" name="acheivement" />
-          <label htmlFor="date">Year of Acheivement</label>
-          <input type="text" name="date" />
+          <input
+            type="text"
+            name="uni"
+            value={uni}
+            onChange={(e) => setUni(e.target.value)}
+          />
+          <label htmlFor="acheivement">Achievement</label>
+          <input
+            type="text"
+            name="acheivement"
+            value={achieve}
+            onChange={(e) => setAchieve(e.target.value)}
+          />
+          <label htmlFor="date">Year of Achievement</label>
+          <input
+            type="text"
+            name="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
           <input type="submit" className="submit" />
         </form>
       </div>
@@ -23,3 +55,8 @@ const EducationForm = () => {
 };
 
 export default EducationForm;
+
+EducationForm.propTypes = {
+  education: PropTypes.array,
+  setEducation: PropTypes.func,
+};
--- cv-app/src/components/forms/ExperienceForm.jsx
+++ cv-app/src/components/forms/ExperienceForm.jsx
@@ -1,4 +1,22 @@
-const ExperienceForm = () => {
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const ExperienceForm = ({ experience, setExperience }) => {
+  const [company, setCompany] = useState("");
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newObject = {
+      company: company,
+      title: title,
+      date: date,
+      description: description,
+    };
+    setExperience([...experience, newObject]);
+  };
   return (
     <>
       <div className="form-container">
@@ -8,15 +26,35 @@ const ExperienceForm = () => {
           <button className="open">&#8226;</button>
           <button className="delete">&#x2715;</button>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="company">Company</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="company"
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+          />
           <label htmlFor="title">Title</label>
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
           <label htmlFor="date">Start Date - End Date</label>
-          <input type="text" name="date" />
+          <input
+            type="text"
+            name="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
           <label htmlFor="description">Description</label>
-          <input type="text" name="description" />
+          <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
           <input type="submit" className="submit" />
         </form>
       </div>
@@ -25,3 +63,8 @@ const ExperienceForm = () => {
 };
 
 export default ExperienceForm;
+
+ExperienceForm.propTypes = {
+  experience: PropTypes.array,
+  setExperience: PropTypes.func,
+};
