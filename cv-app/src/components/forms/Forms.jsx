6d2962b28db91f3ca4c85d9f2b4150ee17350c02
dmychel <dmychel@tutanota.com>
2023-12-13 19:49:05 -0500

    validated props and sent to nested components

--- cv-app/src/components/forms/Forms.jsx
+++ cv-app/src/components/forms/Forms.jsx
@@ -2,17 +2,32 @@ import PersonalForm from "./PersonalForm";
 import ExperienceForm from "./ExperienceForm";
 import EducationForm from "./EducationForm";
 import "../../styles/forms.css";
+import PropTypes from "prop-types";
 
-const Forms = () => {
+const Forms = ({
+  setPersonal,
+  experience,
+  setExperience,
+  education,
+  setEducation,
+}) => {
   return (
     <>
       <section className="forms-master">
-        <PersonalForm />
-        <ExperienceForm />
-        <EducationForm />
+        <PersonalForm setPersonal={setPersonal} />
+        <ExperienceForm experience={experience} setExperience={setExperience} />
+        <EducationForm education={education} setEducation={setEducation} />
       </section>
     </>
   );
 };
 
 export default Forms;
+
+Forms.propTypes = {
+  setPersonal: PropTypes.func,
+  experience: PropTypes.array,
+  setExperience: PropTypes.func,
+  education: PropTypes.array,
+  setEducation: PropTypes.func,
+};
