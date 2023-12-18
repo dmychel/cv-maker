8146c3ef9d4851860d34b8e46151ead4d8b7f3fc
dmychel <dmychel@tutanota.com>
2023-12-17 21:42:09 -0500

    Prop and map
    Sent Props and utilize map function to render prop object

--- cv-app/src/components/resume/ExperienceResume.jsx
+++ cv-app/src/components/resume/ExperienceResume.jsx
@@ -1,14 +1,22 @@
-const ExperienceResume = () => {
+import PropTypes from "prop-types";
+
+const ExperienceResume = ({ experience }) => {
   return (
     <>
-      <h2>Company Name</h2>
-      <p>01/2020 - PRES</p>
-      <p>
-        Lorem ipsum is placeholder text commonly used in the graphic, print, and
-        publishing industries for previewing layouts and visual mockups.
-      </p>
+      <h2>Experience</h2>
+      {experience.map((item) => (
+        <div className="experience" key={item.id}>
+          <h3>{item.company}</h3>
+          <p>{item.date}</p>
+          <p>{item.description}</p>
+        </div>
+      ))}
     </>
   );
 };
 
 export default ExperienceResume;
+
+ExperienceResume.propTypes = {
+  experience: PropTypes.array,
+};
