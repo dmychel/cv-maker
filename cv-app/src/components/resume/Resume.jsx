a7c11a4a931b12936e6d147fa6c27174961052f9
dmychel <dmychel@tutanota.com>
2023-12-17 21:43:15 -0500

    Props

--- cv-app/src/components/resume/Resume.jsx
+++ cv-app/src/components/resume/Resume.jsx
@@ -2,17 +2,24 @@ import PersonalResume from "./PersonalResume";
 import ExperienceResume from "./ExperienceResume";
 import EducationResume from "./EducationResume";
 import "../../styles/resume.css";
+import PropTypes from "prop-types";
 
-const Resume = () => {
+const Resume = ({ personal, experience, education }) => {
   return (
     <>
       <section className="master-resume">
-        <PersonalResume />
-        <ExperienceResume />
-        <EducationResume />
+        <PersonalResume personal={personal} />
+        <ExperienceResume experience={experience} />
+        <EducationResume education={education} />
       </section>
     </>
   );
 };
 
 export default Resume;
+
+Resume.propTypes = {
+  personal: PropTypes.object,
+  experience: PropTypes.array,
+  education: PropTypes.array,
+};
